dae9edb35f849bfefd8fad5c4a9a2d876c47f796
Mahadev Biradar <liaisonbank2007@gmail.com>
2026-06-24 13:55:17 +0530

    Scroll_Top

--- components/ScrollToTopButton.jsx
+++ components/ScrollToTopButton.jsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { useEffect, useState } from "react";
-// import Image from "next/image";
-// import rocketImg from "@/assets/images/rocket.gif";
+import Image from "next/image";
+
 
 export default function ScrollToTopButton() {
   const [visible, setVisible] = useState(false);
+  const scrollTop = "/images/scroll_top.png";
 
   useEffect(() => {
     const onScroll = () => {
@@ -33,7 +34,7 @@ export default function ScrollToTopButton() {
       className="scroll-top scroll-visible"
       type="button"
     >
-      <svg
+      {/* <svg
         xmlns="http://www.w3.org/2000/svg"
         width="48"
         height="48"
@@ -45,15 +46,15 @@ export default function ScrollToTopButton() {
           fillRule="evenodd"
           d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"
         />
-      </svg>
+      </svg> */}
 
-      {/* <Image
-        src={rocketImg}
+      <Image
+        src={scrollTop}
         alt="Back to top"
         width={50}
         height={50}
         priority
-      /> */}
+      />
     </button>
   );
 }
