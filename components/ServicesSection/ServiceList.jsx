cbcb1763c0252a33a2d1ad8c9127c89c871d69ea
Mahadev Biradar <liaisonbank2007@gmail.com>
2026-07-09 17:31:22 +0530

    scroll top issue

--- components/ServicesSection/ServiceList.jsx
+++ components/ServicesSection/ServiceList.jsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+
 import Image from "next/image";
 import rightTick from "@/assets/images/rightTick.svg";
 import { services } from "@/lib/data/servicesData";
@@ -8,23 +9,43 @@ import ServiceContent from "./ServiceContent";
 
 export default function ServiceList({ activeIndex, onChange }) {
   const itemRefs = useRef([]);
-
   const handleClick = (index) => {
-    // Scroll only on mobile/tablet
-    if (window.innerWidth < 1200) {
-     itemRefs.current[index]?.scrollIntoView({
-        behavior: "smooth",
-        block: "center",
-      });
-
-      window.scrollBy({
-        top: -80, // Adjust to your header height
-        behavior: "smooth",
-      });
-    }
-
     onChange(index);
   };
+  // const handleClick = (index) => {
+  //   // Scroll only on mobile/tablet
+  //   if (window.innerWidth < 1200) {
+  //    itemRefs.current[index]?.scrollIntoView({
+  //       behavior: "smooth",
+  //       block: "center",
+  //     });
+
+  //     window.scrollBy({
+  //       top: -80, // Adjust to your header height
+  //       behavior: "smooth",
+  //     });
+  //   }
+
+  //   onChange(index);
+  // };
+  useEffect(() => {
+  if (window.innerWidth >= 1200) return;
+
+  const element = itemRefs.current[activeIndex];
+  if (!element) return;
+
+  const HEADER_HEIGHT = 80;
+
+  const y =
+    element.getBoundingClientRect().top +
+    window.scrollY -
+    HEADER_HEIGHT;
+
+  window.scrollTo({
+    top: y,
+    behavior: "smooth",
+  });
+}, [activeIndex]);
 
   return (
     <ul className="service-list list-unstyled list-icon style-1">
